c584386c10c5c8bca7834bdbe2e65895f0cdb355
Ffion Erin <ffionb@btinternet.com>
2023-01-02 19:07:54 +0000

    completed README and added comments to js file

--- index.js
+++ index.js
@@ -1,169 +1,167 @@
 var finances = [
-['Jan-2010', 867884],
-['Feb-2010', 984655],
-['Mar-2010', 322013],
-['Apr-2010', -69417],
-['May-2010', 310503],
-['Jun-2010', 522857],
-['Jul-2010', 1033096],
-['Aug-2010', 604885],
-['Sep-2010', -216386],
-['Oct-2010', 477532],
-['Nov-2010', 893810],
-['Dec-2010', -80353],
-['Jan-2011', 779806],
-['Feb-2011', -335203],
-['Mar-2011', 697845],
-['Apr-2011', 793163],
-['May-2011', 485070],
-['Jun-2011', 584122],
-['Jul-2011', 62729],
-['Aug-2011', 668179],
-['Sep-2011', 899906],
-['Oct-2011', 834719],
-['Nov-2011', 132003],
-['Dec-2011', 309978],
-['Jan-2012', -755566],
-['Feb-2012', 1170593],
-['Mar-2012', 252788],
-['Apr-2012', 1151518],
-['May-2012', 817256],
-['Jun-2012', 570757],
-['Jul-2012', 506702],
-['Aug-2012', -1022534],
-['Sep-2012', 475062],
-['Oct-2012', 779976],
-['Nov-2012', 144175],
-['Dec-2012', 542494],
-['Jan-2013', 359333],
-['Feb-2013', 321469],
-['Mar-2013', 67780],
-['Apr-2013', 471435],
-['May-2013', 565603],
-['Jun-2013', 872480],
-['Jul-2013', 789480],
-['Aug-2013', 999942],
-['Sep-2013', -1196225],
-['Oct-2013', 268997],
-['Nov-2013', -687986],
-['Dec-2013', 1150461],
-['Jan-2014', 682458],
-['Feb-2014', 617856],
-['Mar-2014', 824098],
-['Apr-2014', 581943],
-['May-2014', 132864],
-['Jun-2014', 448062],
-['Jul-2014', 689161],
-['Aug-2014', 800701],
-['Sep-2014', 1166643],
-['Oct-2014', 947333],
-['Nov-2014', 578668],
-['Dec-2014', 988505],
-['Jan-2015', 1139715],
-['Feb-2015', 1029471],
-['Mar-2015', 687533],
-['Apr-2015', -524626],
-['May-2015', 158620],
-['Jun-2015', 87795],
-['Jul-2015', 423389],
-['Aug-2015', 840723],
-['Sep-2015', 568529],
-['Oct-2015', 332067],
-['Nov-2015', 989499],
-['Dec-2015', 778237],
-['Jan-2016', 650000],
-['Feb-2016', -1100387],
-['Mar-2016', -174946],
-['Apr-2016', 757143],
-['May-2016', 445709],
-['Jun-2016', 712961],
-['Jul-2016', -1163797],
-['Aug-2016', 569899],
-['Sep-2016', 768450],
-['Oct-2016', 102685],
-['Nov-2016', 795914],
-['Dec-2016', 60988],
-['Jan-2017', 138230],
-['Feb-2017', 671099]
-];
-
-console.log("Financial Analysis");
-console.log("-----------------------------");
-// The total number of months included in the dataset.
-console.log("Total months: " + finances.length);
-
-// Created an array which includes only the numbers from the finances array:
-var numbersOnly = []
-for (var i = 0; i < finances.length; i++) {
-    numbersOnly.push (finances[i][1]);
-}
- 
-// Calculated the net total amount of Profit/Losses over the entire period and logged to console.
-var sum = numbersOnly.reduce(function(a, b) { return a + b; }, 0);
-console.log("Total: $" + sum);
-
-// created a new array consisting of the changes between one month to another
-var monthlyChanges = []
-for (var i = 0; i < numbersOnly.length-1; i++) {
-monthlyChanges.push (numbersOnly[i+1]-numbersOnly[i])}
-// console.log (monthlyChanges)
-
-// summed monthly changes 
-var sumOfMonthlyChanges = monthlyChanges.reduce(function(a, b) { return a + b; }, 0);
-// console.log(sumOfMonthlyChanges) to check the sum
-// divided by the number of elements within the monthlyChanges array. rounded to 2 decimal places using "to fixed(2)"
-let averageChanges = sumOfMonthlyChanges / (monthlyChanges.length);
-console.log("Average Change: $" + averageChanges.toFixed(2));
-
-// definted two new variables - 
-// One to hold the greatest increase in change
-// and another to hold an index number so that the corresponding month can console logged
-var greatestIncrease = 0;
-var greatestIncreaseMonth = 0;
-
-// created a for loop to find the biggest number within the monthlChange array
-for (var i = 0; i < monthlyChanges.length; i++) {
-    if(monthlyChanges[i]>greatestIncrease)
-    {
-        greatestIncrease = monthlyChanges[i]
-        // saved the index number (+1 to account for the 'monthlyChanges' array having one less element than the 'finances' array) to the 'greatestIncreaseMonth' variable so this can be printed in the console log)
-        greatestIncreaseMonth = i+1;
-    }
-}
-// console logged some string, the largest increase in profits variable, along with the corresponding month from the finances array - ([0] included so that just the month would be logged, not the profit/loss for that month)
-console.log("Greatest Increase in Profits: " + finances[greatestIncreaseMonth][0] + " ($" + greatestIncrease + ")" );
-
-
-// repeated above steps for finding greatest decrease
-var greatestDecrease = 0;
-var greatestDecreaseMonth = 0;
-
-for (var i = 0; i < monthlyChanges.length; i++) {
-  if(monthlyChanges[i]<greatestDecrease)
-  {
-    greatestDecrease = monthlyChanges[i]
-    greatestDecreaseMonth = i+1;
+  ['Jan-2010', 867884],
+  ['Feb-2010', 984655],
+  ['Mar-2010', 322013],
+  ['Apr-2010', -69417],
+  ['May-2010', 310503],
+  ['Jun-2010', 522857],
+  ['Jul-2010', 1033096],
+  ['Aug-2010', 604885],
+  ['Sep-2010', -216386],
+  ['Oct-2010', 477532],
+  ['Nov-2010', 893810],
+  ['Dec-2010', -80353],
+  ['Jan-2011', 779806],
+  ['Feb-2011', -335203],
+  ['Mar-2011', 697845],
+  ['Apr-2011', 793163],
+  ['May-2011', 485070],
+  ['Jun-2011', 584122],
+  ['Jul-2011', 62729],
+  ['Aug-2011', 668179],
+  ['Sep-2011', 899906],
+  ['Oct-2011', 834719],
+  ['Nov-2011', 132003],
+  ['Dec-2011', 309978],
+  ['Jan-2012', -755566],
+  ['Feb-2012', 1170593],
+  ['Mar-2012', 252788],
+  ['Apr-2012', 1151518],
+  ['May-2012', 817256],
+  ['Jun-2012', 570757],
+  ['Jul-2012', 506702],
+  ['Aug-2012', -1022534],
+  ['Sep-2012', 475062],
+  ['Oct-2012', 779976],
+  ['Nov-2012', 144175],
+  ['Dec-2012', 542494],
+  ['Jan-2013', 359333],
+  ['Feb-2013', 321469],
+  ['Mar-2013', 67780],
+  ['Apr-2013', 471435],
+  ['May-2013', 565603],
+  ['Jun-2013', 872480],
+  ['Jul-2013', 789480],
+  ['Aug-2013', 999942],
+  ['Sep-2013', -1196225],
+  ['Oct-2013', 268997],
+  ['Nov-2013', -687986],
+  ['Dec-2013', 1150461],
+  ['Jan-2014', 682458],
+  ['Feb-2014', 617856],
+  ['Mar-2014', 824098],
+  ['Apr-2014', 581943],
+  ['May-2014', 132864],
+  ['Jun-2014', 448062],
+  ['Jul-2014', 689161],
+  ['Aug-2014', 800701],
+  ['Sep-2014', 1166643],
+  ['Oct-2014', 947333],
+  ['Nov-2014', 578668],
+  ['Dec-2014', 988505],
+  ['Jan-2015', 1139715],
+  ['Feb-2015', 1029471],
+  ['Mar-2015', 687533],
+  ['Apr-2015', -524626],
+  ['May-2015', 158620],
+  ['Jun-2015', 87795],
+  ['Jul-2015', 423389],
+  ['Aug-2015', 840723],
+  ['Sep-2015', 568529],
+  ['Oct-2015', 332067],
+  ['Nov-2015', 989499],
+  ['Dec-2015', 778237],
+  ['Jan-2016', 650000],
+  ['Feb-2016', -1100387],
+  ['Mar-2016', -174946],
+  ['Apr-2016', 757143],
+  ['May-2016', 445709],
+  ['Jun-2016', 712961],
+  ['Jul-2016', -1163797],
+  ['Aug-2016', 569899],
+  ['Sep-2016', 768450],
+  ['Oct-2016', 102685],
+  ['Nov-2016', 795914],
+  ['Dec-2016', 60988],
+  ['Jan-2017', 138230],
+  ['Feb-2017', 671099]
+  ];
+  
+  console.log("Financial Analysis");
+  console.log("-----------------------------");
+  // The total number of months included in the dataset.
+  console.log("Total months: " + finances.length);
+  
+  // Created an array which includes only the numbers from the finances array, and one that includes only the months:
+  var numbersOnly = []
+  for (var i = 0; i < finances.length; i++) {
+      numbersOnly.push (finances[i][1]);
   }
-}
-
-console.log("Greatest Decrease in Profits: " + finances[greatestDecreaseMonth][0] + " ($" + greatestDecrease + ")")
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-// }
-// console.log(changes);
-// The greatest increase in profits (date and amount) over the entire period.- how do I get the date an amount when searching through only for numbers 
-// loop through and stop at the biggest one?
-
-// The greatest decrease in losses (date and amount) over the entire period.
+  
+  
+  // Calculated the net total amount of Profit/Losses over the entire period and logged to console.
+  var sum = numbersOnly.reduce(function(a, b) { return a + b; }, 0);
+  console.log("Total: $" + sum);
+  
+  // created a new array consisting of the changes between one month to another
+  var monthlyChanges = []
+  for (var i = 0; i < numbersOnly.length-1; i++) {
+  monthlyChanges.push (numbersOnly[i+1]-numbersOnly[i])}
+  // console.log (monthlyChanges)
+  
+  // summed monthly changes 
+  var sumOfMonthlyChanges = monthlyChanges.reduce(function(a, b) { return a + b; }, 0);
+  // console.log(sumOfMonthlyChanges) to check the sum
+  // divided by the number of elements within the monthlyChanges array. rounded to 2 decimal places using "to fixed(2)"
+  let averageChanges = sumOfMonthlyChanges / (monthlyChanges.length);
+  console.log("Average Change: $" + averageChanges.toFixed(2));
+  
+  // defined two new variables - 
+  // One to hold the greatest increase in change
+  // and another to hold an index number so that the corresponding month can console logged
+  var greatestIncrease = 0;
+  var greatestIncreaseMonth = 0;
+  
+  // created new array containing only the months from the "finances" array so it can be logged in console.
+  // started the 'for loop' for the array at i = 1 as first element cannot be the biggest increase of decrease in profit
+  var monthsOnly = []
+  for (var i = 1; i < finances.length; i++) {
+      monthsOnly.push (finances[i][0]);
+  }
+  
+  // created a for loop to find the biggest number within the monthlChange array
+  for (var i = 0; i < monthlyChanges.length; i++) {
+      if(monthlyChanges[i]>greatestIncrease)
+      {
+          greatestIncrease = monthlyChanges[i]
+          // saved the index number (+1 to account for the 'monthlyChanges' array having one less element than the 'finances' array) to the 'greatestIncreaseMonth' variable so this can be printed in the console log)
+          greatestIncreaseMonth = i;
+      }
+  }
+  // console logged some string, the largest increase in profits variable, along with the corresponding month from the finances array
+  console.log("Greatest Increase in Profits: " + monthsOnly[greatestIncreaseMonth] + " ($" + greatestIncrease + ")" );
+  
+  
+  // repeated above steps for finding greatest decrease
+  var greatestDecrease = 0;
+  var greatestDecreaseMonth = 0;
+  
+  for (var i = 0; i < monthlyChanges.length; i++) {
+    if(monthlyChanges[i]<greatestDecrease)
+    {
+      greatestDecrease = monthlyChanges[i]
+      greatestDecreaseMonth = i;
+    }
+  }
+  
+  console.log("Greatest Decrease in Profits: " + monthsOnly[greatestDecreaseMonth] + " ($" + greatestDecrease + ")")
+  
+  
+  
+  
+   
+  
+  
+  
+  
+  
